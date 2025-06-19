31ff69c304cce92eef7113ccf953900388d77cce
Ashuatray <Ashu.atray143@gmail.com>
2025-06-19 14:50:07 +0530

    another test case add in add file

--- tests/add.spec.js
+++ tests/add.spec.js
@@ -1,6 +1,16 @@
 const {test,expect}=require('@playwright/test')
 
-test.only('add test ', async ({page}) => {
+test('add test ', async ({page}) => {
+
+    let a = 20;
+    let b = 10;
+    let c = a + b;
+
+    expect(c).toBe(a+b);
+    
+    
+})
+test('add test ', async ({page}) => {
 
     let a = 20;
     let b = 10;
